48a00cc79bb704b26a8cc3eb21e1bbe8de67197c
Albhox <albertotono3@gmail.com>
2020-03-02 11:27:48 -0800

    Bootstrap Registration and Sign-in
    Improvement to the website, still missing main points for Porject 1

--- application.py
+++ application.py
@@ -28,14 +28,24 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
+    # login = db.execute("SELECT username, password FROM users").fetchone()
+    # if login is None:
+    #    return render_template("error.html", message="login or password doesn't match")
     return render_template("index.html")
     # SELECT * FROM users WHERE ( username = username) AND (password = password)
     # SELECT * FROM users WHERE ( username = 'Alberto') AND (password = 'test')
+
+@app.route("/registration")
+def registration():
+    # register = db.execute("INSERT INTO users (username, password) VALUES (:username, :password) ")
+    return render_template("registration.html")
+#INSERT INTO THE DATABASE username and password
 
 @app.route("/hello", methods=["POST"])
 def hello():
     name = request.form.get("name")
     return render_template("hello.html", name=name)
+#SELECT FROM THE DATABASE users
 
 @app.route("/review", methods=["GET", "POST"])
 def review():
@@ -59,19 +69,58 @@ def search() :
     books = db.execute("SELECT * FROM books").fetchall()
     return render_template("search.html", books=books)
 
+@app.route("/search/<int:book_isbn>")
+def book(book_isbn):
+    """Lists details about a single book."""
+# Make sure book exists.
+    book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_isbn}).fetchone()
+    if book is None:
+        return render_template("error.html", message="No such book.")
+    return render_template("book.html", book=book)
+
+# Get all reviews.
+    reviews = db.execute("SELECT name FROM reviews WHERE book_isbn = :book_isbn",
+                            {"book_isbn": book_isbn}).fetchall()
+    return render_template("book.html", book=book, reviews=reviews)
+
+
+# Get Success or Error while searching for the book
+
 @app.route("/searchbook", methods=["POST"])
 def searchbook():
     """Search a book."""
 
-    # Get form information.
+    # Get form information and set up Error Message
     name = request.form.get("name")
     try:
         book_isbn = int(request.form.get("book_isbn"))
     except ValueError:
-        return render_template("error.html", message="Invalid book number.")
+        return render_template("error.html", message="Invalisbn book number.")
 
-    # Make sure the flight exists. Complete the if statement with a positive end
+    # Make sure the book exists. Complete the if statement with a positive end
     if db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_isbn}).rowcount == 0:
-        return render_template("error.html", message="No such book with that id.")
+        return render_template("error.html", message="No such book with that isbn.")
+    # db.execute("SELECT * FROM books (name, book_isbn) VALUES (:name, :book_isbn)",
+    #         {"name": name, "book_isbn": book_isbn})
     db.commit()
     return render_template("success.html")
+
+# @app.route("/books")
+# def books():
+#     """Lists all books."""
+#     books = db.execute("SELECT * FROM books").fetchall()
+#     return render_template("books.html", books=books)
+
+# @app.route("/books/<int:book_isbn>")
+# def book(book_isbn):
+#     """Lists details about a single book."""
+# Make sure book exists.
+    # book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": book_isbn}).fetchone()
+    # if book is None:
+    #     return render_template("error.html", message="No such book.")
+    # return render_template("book.html", book=book)
+
+# Get all reviews.
+    # reviews = db.execute("SELECT name FROM reviews WHERE book_isbn = :book_isbn", {"book_isbn": book_isbn}).fetchall()
+    # return render_template("book.html", book=book, reviews=reviews)
+    
--- create.sql
+++ create.sql
@@ -5,3 +5,15 @@ CREATE TABLE books (
     author VARCHAR NOT NULL,
     year INTEGER NOT NULL
 );
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    review VARCHAR NOT NULL,
+    stars INTEGER NOT NULL
+);
